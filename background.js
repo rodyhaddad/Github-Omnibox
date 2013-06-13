48320f9186c263b34a7eac14bb3e7ab843e27374
Dean Sofer <Dean@Dean-Paxata.local>
2013-06-13 13:55:48 -0700

    Prototyping new switching code

--- background.js
+++ background.js
@@ -23,11 +23,14 @@ var tools = {
       });
     });
   },
+  my: function(text) {
+    return text.match(/my\s+([\w-]+)?/);
+  },
   user: function(text) {
     return text.match(/@([\w-]+)/);
   },
   repo: function(text) {
-    return text.match(/([\w-]+\/[\w-]+)(\s+.+)?/);
+    return text.match(/([\w-]+\/[\w-\.]+)(\s+.+)?/);
   },
   repoAction: function(text) {
     return text.match(/(pulse|wiki|pulls|graphs|network|issues|admin|travis|new pull|new issue|#([0-9]+)|@([\w-]+))/);
@@ -53,12 +56,14 @@ var tools = {
 function setDefault(text, suggest) {
   var type, action, description = 'Go to ';
 
-  if (type = tools.isUrl(text)) {
+  if (type = tools.my(text)) {
+    suggestMine(type, suggest);
+  } else if (type = tools.isUrl(text)) {
     description = 'Go to';
   } else if (type = tools.user(text)) {
     description += 'user';
   } else if (type = tools.repo(text)) {
-    suggestActions(text, suggest);
+    suggestActions(type, suggest);
     if (type[2] && (action = tools.repoAction(type[2]))) {
       if (action[2]) {
         description += 'repo issue';
@@ -91,18 +96,34 @@ function setSuggestions(text, suggest) {
   ]);
 }
 
-function suggestActions(text, suggest) {
+function suggestActions(repo, suggest) {
   var suggestions = [],
     description = 'Go to ',
-    content = base + text + '/',
+    content = base + repo[1] + '/',
     actions = ['pulse','wiki','pulls','graphs','network','issues','admin'];
 
   for (var i = actions.length - 1; i >= 0; i--) {
-    suggestions.push({ description: description + actions[i] + ': ' + text, content: content + actions[i] });
+    suggestions.push({ description: description + actions[i] + ': ' + repo[1], content: content + actions[i] });
   };
-  suggestions.push({ description: 'New pull: ' + text, content: content + 'issues/new' });
-  suggestions.push({ description: 'New issue: ' + text, content: content + 'pulls/new' });
-  suggestions.push({ description: 'Go to travis: ' + text, content: travisBase + text });
+  suggestions.push({ description: 'New pull: ' + repo[1], content: content + 'issues/new' });
+  suggestions.push({ description: 'New issue: ' + repo[1], content: content + 'pulls/new' });
+  suggestions.push({ description: 'Go to travis: ' + repo[1], content: travisBase + repo[1] });
+
+  suggest(suggestions);
+}
+
+function suggestMine(mine, suggest) {
+  var suggestions = [],
+    description = 'Go to ',
+    content = base + repo[1] + '/',
+    actions = ['dashboard','settings','notifications','stars'];
+
+  for (var i = actions.length - 1; i >= 0; i--) {
+    suggestions.push({ description: description + actions[i] + ': ' + repo[1], content: content + actions[i] });
+  };
+  suggestions.push({ description: 'New pull: ' + repo[1], content: content + 'issues/new' });
+  suggestions.push({ description: 'New issue: ' + repo[1], content: content + 'pulls/new' });
+  suggestions.push({ description: 'Go to travis: ' + repo[1], content: travisBase + repo[1] });
 
   suggest(suggestions);
 }
@@ -112,6 +133,63 @@ var global = {
   users: [],
   issues: []
 };
+
+
+
+function switcher(switches, text) {
+  switches.each(function(item){
+    var match;
+    if (match = text.match(item.pattern))
+      item.next(match);
+  });
+}
+var switches = [], mySwitches = [], repoSwitches = [], suggest, repo;
+mySwitches.push({ pattern: 'pulls', next: function(match){
+  tools.navigate('dashboard/pulls');
+}});
+mySwitches.push({ pattern: 'issues', next: function(match){
+  tools.navigate('dashboard/issues');
+}});
+mySwitches.push({ pattern: /^dashboard|stars|profile|settings|notifications/, next: function(match){
+  tools.navigate(match[0]);
+}});
+repoSwitches.push({ pattern: /^wiki|pulse|network|graphs|pulls/, next: function(match){
+  tools.navigate(repo + '/' + match[0]);
+}});
+repoSwitches.push({ pattern: /^new (issue|pull)/, next: function(match){
+  tools.navigate(repo + match[1] + 's/new');
+}});
+repoSwitches.push({ pattern: /(@([\w-.]+))?\s*(\/.*)?/, next: function(match){
+  var tree = match[2] || 'tree',
+      path = match[3] || '';
+  tools.navigate(repo + '/' + tree + path);
+}});
+repoSwitches.push({ pattern: /^issues|#([0-9]+)?/, next: function(match){
+  var number = match[1] || '';
+  tools.navigate(repo + '/issues/' + number);
+}});
+repoSwitches.push({ pattern: /^admin|settings/, next: function(match){
+  tools.navigate(repo + '/settings');
+}});
+switches.push({ pattern: /^my\s+(\w+.*)/, next: function(match){
+  switcher(mySwitches, match[1]);
+}});
+switches.push({ pattern: /^@(\w+)/, next: function(match){
+  tools.navigate(match[1]);
+}});
+switches.push({ pattern: /^[\w-]+\/[\S]+/, next: function(match){
+  tools.navigate(match);
+}});
+switches.push({ pattern: /^([\w-]+\/[\S]+)\s(.*)/, next: function(match){
+  repo = match[1];
+  switcher(repoSwitches, match[2]);
+}});
+switches.push({ pattern: /^https?:/, next: function(match){
+  tools.navigate(match[0], true);
+}});
+switches.push({ pattern: /.+/, next: function(match){
+  tools.navigate(match[1]);
+}});
 
 /*chrome.omnibox.onInputStarted.addListener(function(){
   var repo, repos = tools.http('https://github.com/command_bar/repos');
@@ -140,7 +218,9 @@ chrome.omnibox.onInputChanged.addListener(
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(function(text) {
   var type, action;
-  if (type = tools.isUrl(text)) {
+  if (type = tools.my(text)) {
+    tools.navigate
+  } else if (type = tools.isUrl(text)) {
     tools.navigate(text);
   } else if (type = tools.user(text)) {
     tools.navigate(type[1]);
